b5dfa1810a0aa9ad01ece52b898c3da46024d259
Spencer Miskoviak <smiskoviak@gmail.com>
2019-08-04 12:33:39 -0700

    Update the styling

--- basic/src/components/heading.jsx
+++ basic/src/components/heading.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
-const Heading = props => <h1>{props.children}</h1>;
+const Heading = props => (
+  <h1 style={{ textAlign: "center", color: "#333", fontFamily: "sans-serif" }}>
+    {props.children}
+  </h1>
+);
 
 export default Heading;
